93c3ff189e632ec2d46f8fa472a5c87d738f615c
musleh0001 <khan.muslehuddin0001@gmail.com>
2020-08-10 15:54:41 +0600

    new branch

--- account/views.py
+++ account/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Welcome to Mysite")
